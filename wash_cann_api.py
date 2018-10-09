c50cd2b425fbb758abad63ca13ebd20c20c78691
Jawaun <jawaun.brown95@gmail.com>
2018-10-09 14:47:27 -0400

    added table of most common business type per county

--- wash_cann_api.py
+++ wash_cann_api.py
@@ -12,26 +12,27 @@ def connect(limiter):
 
     # First 2000 results, returned as JSON from API / converted to Python list of
     # dictionaries by sodapy.
+
     results = client.get('bhbp-x4eb', limit=limiter)
 
     # Convert to pandas DataFrame
-    results_df = pd.DataFrame.from_records(results)
-    cols = results_df.columns
-    types = results_df.dtypes
-    descriptions = results_df.describe()
-    bycounty = results_df.sort_values(by='county')
-    print(cols)
-    print(types)
-    print(descriptions)
-    print(bycounty)
+    df = pd.DataFrame.from_records(results)
 
-    return results_df
+    # cols = df.columns
+    # types = df.dtypes
+    # descriptions = df.describe()
+    # county = df.county.unique()
+    # print(cols)
+    # print(types)
+    # print(descriptions)
+    # print(county)
+    # print(ndf)
 
-
-# def get_schema(df):
-#     pd.build_table_schema(df)
+    return df
 
 
 if __name__ == "__main__":
     d = connect(2000)
     d.to_csv('/Users/Juan/projects/cannadata/cannadata.csv')
+    max_type = d.groupby('county').type.max()
+    max_type.to_csv('/Users/Juan/projects/cannadata/most_common_biz_type_per_county.csv')
